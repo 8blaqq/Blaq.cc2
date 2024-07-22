db983fed50b6adb206b1e287dca7ddf82238448a
8blaqq <habibisonija@gmail.com>
2024-07-22 14:53:52 -0700

    Update licenses

--- licenses.lua
+++ licenses.lua
@@ -1,5 +1,5 @@
 return {
     ['BLAQ-78UZ-YTW1'] = {user_id = 1149023627069620297, hwid = 'None', username = 'xaxaxaxaxaxaxaxaxxaxaxaxaxaxax'},
-    ['BLAQ-PM94-IBXC'] = {user_id = 1210211190379388952, hwid = 'None', username = 'scarsfromlastnight'},
+    ['BLAQ-PM94-IBXC'] = {user_id = 1210211190379388952, hwid = 'BLAQ-PM94-IBXC', username = 'scarsfromlastnight'},
     ['BLAQ-H3C3-581W'] = {user_id = 1167549408963985409, hwid = 'None', username = '8blaq'},
 }
