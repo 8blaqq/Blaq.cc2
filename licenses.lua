a2bee58b89aa7feee4079cb5eaa8953f26acf128
8blaqq <habibisonija@gmail.com>
2024-07-23 16:14:18 -0700

    Update licenses

--- licenses.lua
+++ licenses.lua
@@ -23,7 +23,7 @@ return {
     ['BLAQ-K5I1-3W5T'] = {user_id = 1226664726403088404, hwid = 'None', username = 'dragodragodragodrago'},
     ['BLAQ-RGNN-A4YU'] = {user_id = 794619423616532561, hwid = 'None', username = 'ftsf'},
     ['BLAQ-JRBY-YJNI'] = {user_id = 1144407963646304308, hwid = 'c3953a2b-32ae-49aa-92c5-76528046a0b6', username = 'unknownnalocker'},
-    ['BLAQ-061U-2C1O'] = {user_id = 1106703744458039387, hwid = 'None', username = 'squeeze_benz.'},
+    ['BLAQ-061U-2C1O'] = {user_id = 1106703744458039387, hwid = 'me', username = 'squeeze_benz.'},
     ['BLAQ-DSHK-H8PR'] = {user_id = None, hwid = 'None', username = ''},
     ['BLAQ-QO05-EDXO'] = {user_id = None, hwid = 'None', username = ''},
     ['BLAQ-LDQ5-UN17'] = {user_id = 788780177412194344, hwid = 'None', username = 'juubladi'},
