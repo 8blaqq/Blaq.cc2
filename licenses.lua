64bfd151f3117d834d09e7dfb1698cf3507e2ad4
8blaqq <habibisonija@gmail.com>
2024-07-23 05:04:48 -0700

    Update licenses

--- licenses.lua
+++ licenses.lua
@@ -15,4 +15,5 @@ return {
     ['BLAQ-DUVI-FOWA'] = {user_id = 1210211190379388952, hwid = 'None', username = 'scarsfromlastnight'},
     ['BLAQ-8NY1-OREO'] = {user_id = 1210992444720685138, hwid = 'None', username = 'hejspade'},
     ['BLAQ-TLRE-MVFT'] = {user_id = 782372822831595580, hwid = 'None', username = 'colloperr'},
+    ['BLAQ-FXY1-9E6R'] = {user_id = 731826231724605480, hwid = 'None', username = 'gasdhasd2123rwfas'},
 }
