316caba3b1b0c40b7089b309d64eedadbc6e75ff
8blaqq <habibisonija@gmail.com>
2024-07-23 16:47:30 -0700

    Update licenses

--- licenses.lua
+++ licenses.lua
@@ -140,5 +140,5 @@ return {
     ['BLAQ-JPMM-TD67'] = {user_id = None, hwid = 'None', username = ''},
     ['BLAQ-J06E-QYLL'] = {user_id = None, hwid = 'None', username = ''},
     ['BLAQ-3ON2-7XFL'] = {user_id = None, hwid = 'None', username = ''},
-    ['BLAQ-AI1O-4NK4'] = {user_id = 1229518289185669142, hwid = 'None', username = 'dtbtree'},
+    ['BLAQ-AI1O-4NK4'] = {user_id = 1229518289185669142, hwid = 'b2c4c5e3-ac64-4e97-8608-e960eb2af8fb', username = 'dtbtree'},
 }
